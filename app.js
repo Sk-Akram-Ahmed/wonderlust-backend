f687916efc87279542785346ddc0e6080339df98
Sk-Akram <its,shaik.akram@gamil.com>
2026-06-25 07:08:09 +0530

    First commmit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const path = require('path');
+app.set("view engine" , " ejs");
+app.set("views",path.join(__dirname,"views"))
+main().then(()=>{
+    console.log('Connected to the Database');
+
+})
+.catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
+}
+const Listing =require('./models/listing.js');
+
+
+app.get('/listings',async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+})
+
+app.get
+app.listen(8080,()=>{
+    console.log('Server is running on the port 8080');
+})
---
+++ init/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const initData = require('./data.js');
+const Listing = require('../models/listing.js');
+main().then(()=>{
+    console.log('Connected to the Database');
+
+})
+.catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+console.log('Database Initialized with Data');
+}
+
+
+initDB();
+
+
+
+
+
+
+
+
+
+
+
