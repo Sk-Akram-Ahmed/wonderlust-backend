ea55faa8ffedbed340fb4f1f51ae0f4561b79510
Sk-Akram <its,shaik.akram@gamil.com>
2026-06-25 20:29:26 +0530

    added all routes

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 const path = require('path');
+const methodOverride = require("method-override");
 app.set("view engine" , " ejs");
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 main().then(()=>{
     console.log('Connected to the Database');
 
@@ -39,11 +41,28 @@ app.post("/listings",async(req,res)=>{
     const newListing = new Listing(req.body.listings);
     await newListing.save();
     res.redirect("/listings");
-}
-    
-)
+})
 
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    const {id} = req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing})
+})
 
+//update route 
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+// delete route
+app.delete("/listings/:id",async(req,res)=>{
+    const {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
 
 app.get
 app.listen(8080,()=>{
