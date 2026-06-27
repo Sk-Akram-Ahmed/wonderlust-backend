bfe184002739718b15a2b9923066d67be34d8ae1
Sk-Akram <its,shaik.akram@gamil.com>
2026-06-28 00:13:49 +0530

    added styling

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app=express();
 const mongoose=require('mongoose');
 const path = require('path');
 const methodOverride = require("method-override");
-app.set("view engine" , " ejs");
+const ejsMate= require("ejs-mate");
+app.engine("ejs",ejsMate); 
+app.set("view engine" , "ejs");
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"public")));
+
 main().then(()=>{
     console.log('Connected to the Database');
 
